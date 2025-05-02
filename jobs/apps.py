d88d08e015e24d464afa2929e3ef9d348bff45bd
kkig <ex@sample.com>
2025-05-02 16:25:07 +0900

    Update to create Profiles on user creation

--- jobs/apps.py
+++ jobs/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class JobsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jobs'
+
+    def ready(self):
+        # This ensures signals are connected when Django starts
+        import jobs.signals
