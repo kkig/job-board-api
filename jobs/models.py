03aa7107bb7b742a14cd15329e30fbd9ef7b2b38
kkig <ex@sample.com>
2025-05-02 14:53:45 +0900

    Add Job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=50, choices=[
+        ('FT', 'Full-time'), ('PT', 'Part-time')]
+    )
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
