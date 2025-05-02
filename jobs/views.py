e255ed8ed7c067ac6527401329889eece8ed5b2e
kkig <ex@sample.com>
2025-05-02 17:32:28 +0900

    Add permission - only employers can post jobs

--- jobs/views.py
+++ jobs/views.py
@@ -2,9 +2,15 @@ from rest_framework import viewsets, permissions
 from .models import Job
 from .serializers import JobSerializer
 
+from .permissions import IsEmployer
+
 
 # Create your views here.
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsEmployer()]
+        return [permissions.AllowAny()]
