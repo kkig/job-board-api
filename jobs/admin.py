170fc6e42388f4d5b0c1e53e8bdf3efd407e8be5
kkig <ex@sample.com>
2025-05-03 12:30:18 +0900

    Register Application to admin page

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Job, Profile
+from .models import Job, Profile, Application
 
 # Register your models here.
 admin.site.register(Job)
 admin.site.register(Profile)
+admin.site.register(Application)
